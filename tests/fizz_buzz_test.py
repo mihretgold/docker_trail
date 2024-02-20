6fe87fdd85cf965c32b01828d728b2f9a6e2b690
mihreta2sv <mihret.agenehu@a2sv.org>
2024-02-20 18:47:41 +0300

    trail

--- tests/fizz_buzz_test.py
+++ tests/fizz_buzz_test.py
@@ -1,5 +1,5 @@
-from .. import main  # Adjust this import based on your actual file names
+from main import fizzBuzz  # Adjust this import based on your actual file names
 
 def test_fizzbuzz():
-    result = main.fizzBuzz(15)
+    result = fizzBuzz(15)
     assert result == ["1", "2", "Fizz", "4", "Buzz", "Fizz", "7", "8", "Fizz", "Buzz", "11", "Fizz", "13", "14", "Fizz Buzz"]
